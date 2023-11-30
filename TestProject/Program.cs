276e9f442927fcc201f08b8b7000a68a2aad9cc2
matanyos <37904698+matanyos@users.noreply.github.com>
2023-11-30 19:06:17 +0100

    added logging to UserController

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -14,6 +14,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddLogging();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
