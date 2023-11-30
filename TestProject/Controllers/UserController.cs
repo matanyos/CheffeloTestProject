00c2a5a08c14bb3357f9f430b085cc82b274add8
matanyos <37904698+matanyos@users.noreply.github.com>
2023-11-30 18:17:52 +0100

    implemented CRUD operations using UserRepository

--- TestProject/Controllers/UserController.cs
+++ TestProject/Controllers/UserController.cs
@@ -1,31 +1,106 @@
 using Microsoft.AspNetCore.Mvc;
 using Work.ApiModels;
+using Work.Database;
+using Work.Interfaces;
 
 namespace Work.Controllers
 {
     [ApiController]
+    [Route("[controller]")]
     public class UserController : ControllerBase
     {
+        private readonly IRepository<User, Guid> userRepository;
 
+        public UserController(IRepository<User, Guid> userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        [HttpGet("{id}")]
         public IActionResult Get(Guid id)
         {
-            throw new NotImplementedException("TODO");
+            try
+            {
+                var user = userRepository.Read(id);
+                return Ok(user);
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound($"User with ID {id} is not found");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
         }
 
-        public IActionResult Post(UserModelDto user)
+        [HttpPost]
+        public IActionResult Post([FromBody] UserModelDto userModelDto)
         {
-            throw new NotImplementedException("TODO");
-        }
-        
-        public IActionResult Put(UserModelDto user)
-        {
-            throw new NotImplementedException("TODO");
+            try
+            {
+                var user = new User
+                {
+                    UserId = Guid.NewGuid(),
+                    UserName = userModelDto.Name,
+                    Birthday = userModelDto.Birthdate
+                };
+
+                userRepository.Create(user);
+
+                return CreatedAtAction(nameof(Get), new { id = user.UserId }, user);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
         }
 
+        [HttpPut]
+        public IActionResult Put([FromBody] UserModelDto userModelDto)
+        {
+            try
+            {
+                var user = new User
+                {
+                    UserId = userModelDto.Id,
+                    UserName = userModelDto.Name,
+                    Birthday = userModelDto.Birthdate
+                };
+
+                userRepository.Update(user);
+
+                return Ok(user);
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound($"User with ID {userModelDto.Id} is not found");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+        [HttpDelete("{id}")]
         public IActionResult Delete(Guid id)
         {
-            throw new NotImplementedException("TODO");
-        }
+            try
+            {
+                var user = userRepository.Read(id);
 
+                userRepository.Remove(user);
+
+                return NoContent();
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound($"User with ID {id} is not found");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
     }
 }
--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,6 +1,13 @@
+using Work.Database;
+using Work.Implementation;
+using Work.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddSingleton(_ => new MockDatabase(10));
+builder.Services.AddSingleton<IRepository<User, Guid>, UserRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
